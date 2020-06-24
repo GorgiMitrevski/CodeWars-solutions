c458da19c4d6a8d71a4e422717dd5b7edb9943a0
GorgiMitrevski <39677262+GorgiMitrevski@users.noreply.github.com>
2020-06-24 17:10:11 +0200

    Create Persistent Bugger.js

---
+++ Persistent Bugger.js
@@ -0,0 +1,12 @@
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
+/ /which is the number of times you must multiply the digits in num until you reach a single digit.
+
+function persistence(num) {
+   let timesMult = 0;
+   num = num.toString();
+   while(num.length > 1){
+     timesMult++;
+     num = num.split('').map(Number).reduce((a,b) => a * b).toString();
+   }
+   return timesMult;
+}
